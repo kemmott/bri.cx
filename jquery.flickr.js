42466ed0359ae98ea91f62761f52636907a5f26b
Kevan Emmott <kevan@emmott.com>
2018-10-12 01:30:13 -0400

    https

--- jquery.flickr.js
+++ jquery.flickr.js
@@ -1,13 +1,13 @@
-/* Flickr Gallery script 
+/* Flickr Gallery script
 * powered by jQuery (http://www.jquery.com)
 
 * written by Richard Shepherd (http://richardshepherd.com)
 
-* for more info visit 
+* for more info visit
 * http://www.richardshepherd.com/how-to-use-jquery-with-a-json-flickr-feed-to-display-photos/ */
 function getFlickr(userid, target, numberToDisplay) {
 
-    var url = "http://api.flickr.com/services/feeds/photos_public.gne?id=" + userid + "&lang=en-us&format=json&jsoncallback=?";
+    var url = "https://api.flickr.com/services/feeds/photos_public.gne?id=" + userid + "&lang=en-us&format=json&jsoncallback=?";
 
     $.getJSON(url, displayImages);
 
@@ -18,15 +18,15 @@ function getFlickr(userid, target, numberToDisplay) {
         $.each(data.items, function(i, item) {
 
             if (ctr < numberToDisplay) {
-                var sourceMed = 
+                var sourceMed =
                     (item.media.m).replace("_m.jpg", "_t.jpg");
 
-                htmlString += '<a href="' + item.link + 
-                    '" title="' + item.title + 
+                htmlString += '<a href="' + item.link +
+                    '" title="' + item.title +
                     '" target="_blank" style="opacity: 1;">';
-                htmlString += '<img title="' + item.title + 
+                htmlString += '<img title="' + item.title +
                     '" src="' + item.media.m + '" ';
-                htmlString += 'alt="' + item.title + 
+                htmlString += 'alt="' + item.title +
                     '" style="opacity: 1;" />';
                 htmlString += '</a>';
                 ctr = ctr + 1
@@ -35,13 +35,13 @@ function getFlickr(userid, target, numberToDisplay) {
 
         $('#' + target).append(htmlString);
 
-        //update image preview so we get 
+        //update image preview so we get
         //the nice popup mouseovers on the images
-        //Note: this uses the Image Preview Script, 
+        //Note: this uses the Image Preview Script,
         //if not using it remove the below line.
 
 
 
-        //imagePreview();            
+        //imagePreview();
     }
 }
--- jquery.flickrsets.js
+++ jquery.flickrsets.js
@@ -1,13 +1,13 @@
-/* Flickr Gallery script 
+/* Flickr Gallery script
 * powered by jQuery (http://www.jquery.com)
 
 * written by Richard Shepherd (http://richardshepherd.com)
 
-* for more info visit 
+* for more info visit
 * http://www.richardshepherd.com/how-to-use-jquery-with-a-json-flickr-feed-to-display-photos/ */
 function getFlickrSets(userid, target, numberToDisplay) {
 
-    var url = "http://api.flickr.com/services/rest/?method=flickr.photosets.getList&api_key=51d1d9fb120a5eb100291180adb3d5b9&user_id=" + userid + "&format=json&jsoncallback=?";
+    var url = "https://api.flickr.com/services/rest/?method=flickr.photosets.getList&api_key=51d1d9fb120a5eb100291180adb3d5b9&user_id=" + userid + "&format=json&jsoncallback=?";
 
     $.getJSON(url, displayImages);
 
@@ -22,30 +22,30 @@ function getFlickrSets(userid, target, numberToDisplay) {
 
             if (ctr < numberToDisplay) {
 
-            var sourceMed = "http://farm" + theset.farm + ".static.flickr.com/" + theset.server + "/" + theset.primary + "_" + theset.secret + "_m.jpg";
-            var setUrl = "http://www.flickr.com/photos/emmott/sets/" + theset.id;
+            var sourceMed = "https://farm" + theset.farm + ".static.flickr.com/" + theset.server + "/" + theset.primary + "_" + theset.secret + "_m.jpg";
+            var setUrl = "https://www.flickr.com/photos/emmott/sets/" + theset.id;
 
-                htmlString += '<a href="' + setUrl + 
-                    '" title="' + theset.title._content + 
+                htmlString += '<a href="' + setUrl +
+                    '" title="' + theset.title._content +
                     '" target="_blank" style="opacity: 1;">';
-                htmlString += '<img title="' + theset.title._content + 
+                htmlString += '<img title="' + theset.title._content +
                     '" src="' + sourceMed + '" ';
-                htmlString += 'alt="' + theset.title._content + 
+                htmlString += 'alt="' + theset.title._content +
                     '" style="opacity: 1;" />';
                 htmlString += '</a>';
                 ctr = ctr + 1
             }
-        }); 
+        });
 
         $('#' + target).append(htmlString);
 
-        //update image preview so we get 
+        //update image preview so we get
         //the nice popup mouseovers on the images
-        //Note: this uses the Image Preview Script, 
+        //Note: this uses the Image Preview Script,
         //if not using it remove the below line.
 
 
 
-        //imagePreview();            
+        //imagePreview();
     }
 }
